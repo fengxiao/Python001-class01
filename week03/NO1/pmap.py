3a4795cd977031c61e530918bedd0374b19864fc
fengxiao <canyoufx@126.com>
2020-07-13 01:32:42 +0800

    完善第三周作业

--- week03/NO1/pmap.py
+++ week03/NO1/pmap.py
@@ -1,10 +1,13 @@
-
+import ipaddress
 import json
 import argparse
 import subprocess
 import sys
+import telnetlib
+from concurrent.futures import ThreadPoolExecutor
 
-def ping(ip):
+
+def pingcheck(ip):
     p = subprocess.Popen(["ping.exe", ip],
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE,
@@ -14,9 +17,36 @@ def ping(ip):
     out = p.stdout.read()
     # print(str(out, "GBK"))
     if "无法访问目标主机" in str(out, "GBK"):
-        print("0")
+        pass
+        # print(f"{ip}访问异常")
     else:
-        print("1")
+        print(f"{ip}——访问正常")
+
+def portcheck(ipandport):
+    [ip, port] = ipandport.split(":")
+    try:
+        tn = telnetlib.Telnet(ip, port)
+        print(f"{ip}:{port}——端口访问正常")
+    except Exception as e:
+        print(f"{ip}:{port}——端口访问异常")
+
+def getiplist(ip):
+    [start, end] = ip.split("-")
+    parts = start.split(".")[:-1]
+    parts.append("0")
+    network = ipaddress.ip_network(".".join(parts) + "/24")
+    iplist = [
+        str(i)
+        for i in network.hosts()
+        if i >= ipaddress.ip_address(start) and i <= ipaddress.ip_address(end)
+    ]
+    return iplist
+
+def getipandportlist(ip):
+    list = []
+    for i in range(75,85):
+        list.append(f"{ip}:{i}")
+    return  list
 
 
 if __name__ == "__main__":
@@ -33,4 +63,11 @@ if __name__ == "__main__":
     ip = args.ip
     filename = args.w
     # print(f"{workers},{scantype},{ip},{filename}")
-    ping(ip)
+    if scantype =="ping":
+        iplist = getiplist(ip)
+        with ThreadPoolExecutor(workers) as executor:
+            executor.map(pingcheck, iplist)
+    if scantype == "tcp":
+        portlist = getipandportlist(ip)
+        with ThreadPoolExecutor(workers) as tcpexecutor:
+            tcpexecutor.map(portcheck, portlist)
--- week03/NO1/testpamp.py
+++ week03/NO1/testpamp.py
@@ -1,2 +1,9 @@
 import os
-mreturn = os.system("pmap.py -n 10 -f tcp -ip 192.168.10.180 -w result.json")
+commamd = "pmap.py -n 4 -f ping -ip 192.168.10.1-192.168.10.20"
+print(commamd)
+mreturn = os.system(commamd)
+
+# commamd = "pmap.py -n 4 -f tcp -ip 192.168.30.16"
+# print(commamd)
+# mreturn = os.system(commamd)
+
